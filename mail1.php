755dfe0f1b0c4d496dc352ee75e6703df23c76ba
BP <bp@BPs-MacBook-Air.local>
2026-07-07 21:32:53 +0530

    Redirect direct GET requests to index.php in mail1.php

--- mail1.php
+++ mail1.php
@@ -8,6 +8,11 @@ require 'PHPMailer/Exception.php';
 
 session_start();
 
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    header("Location: index.php");
+    exit();
+}
+
 $mail = new PHPMailer(true);
 
 // CAPTCHA Validation
